b3be3c37b580ca2c64f8391085e9a82f28760409
JeanGDEV <123316415+JeanGDEV@users.noreply.github.com>
2026-09-16 13:20:15 -0400

    Reference classes in js via dom

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let displayValue = document.querySelector("input")
 
+
+/** NUMBER & OPERATOR BUTTONS */
+let numberButtons = document.querySelectorAll(".digit-btn")
+let operatorButtons = document.querySelectorAll(".operator-btn")
+
+/** SPECIAL BUTTONS */
+let clearButton = document.querySelector(".clear-btn")
+let deleteButton = document.querySelector(".back-btn")
+let equalButton = document.querySelector(".equal-btn")
+
 let currentNumber = 0
 let firstNumber = null
 let operator = null
